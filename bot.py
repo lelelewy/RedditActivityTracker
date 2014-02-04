d87eb6ad3323b535294f6bb6b35eff3622842dcc
lelelewy <lelelewy@gmail.com>
2014-02-04 17:52:47 -0500

    Added functionality for submissions, not just comments

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ r.login()
 user = r.get_redditor('insertusernamehere')
 commentCollection = []
 commentComparison = []
+submissionCollection = []
+submissionComparison = []
 
 def commentMatcher():
 	global commentCollection
@@ -21,6 +23,21 @@ def commentMatcher():
 		r.send_message('insertusernamehere', '%s just made a new comment' %user, commentComparison[0])
 		commentCollection = list(commentComparison)
 
+def submissionMatcher():
+	global submissionCollection
+	global submissionComparison 
+	submissions = user.get_submitted(limit = 1)
+	for submission in submissions:
+		submissionCollection.insert(0,submission)
+	time.sleep(10)
+	submissions = user.get_submitted(limit = 1)
+	for submission in submissions:
+		submissionComparison.insert(0, submission)
+	if submissionCollection[0] != submissionComparison[0]:
+		r.send_message('insertusernamehere', '%s just made a new comment' %user, submissionComparison[0])
+		submissionCollection = list(submissionComparison)
+
 
 while(True):
 	commentMatcher()
+	submissionMatcher()
